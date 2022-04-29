5c8c7d49f655b3cd29ea3c44749a10bed5886808
osorosia <64416398+osorosia@users.noreply.github.com>
2022-04-29 23:13:59 +0900

    add strlen strslen

---
+++ ex00/vendor/piscine/strlen.go
@@ -0,0 +1,9 @@
+package piscine
+
+func Strlen(str string) (i int) {
+	runs := []rune(str)
+
+	for i = range runs {
+	}
+	return i + 1
+}
---
+++ ex00/vendor/piscine/strslen.go
@@ -0,0 +1,7 @@
+package piscine
+
+func Strslen(strs []string) (i int) {
+	for i = range strs {
+	}
+	return i + 1
+}
