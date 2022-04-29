af01f1bac44956d04233199993e8ea6b1c310604
JimpeiYamamoto <zinpei104@gmail.com>
2022-04-30 01:25:29 +0900

    add : TestIsOnlyKing

--- ex00/main_test.go
+++ ex00/main_test.go
@@ -1,8 +1,65 @@
 package main
 
 import (
+	"strings"
 	"testing"
 )
+
+func TestIsOnlyKing(t *testing.T) {
+	trueTest := [][]string{
+		[]string{
+			"R.P.",
+			".K..",
+			"..P.",
+			"....",
+		},
+		[]string{
+			"Kk",
+			"kk",
+		},
+		[]string{
+			"K",
+		},
+	}
+	falseTest := [][]string{
+		[]string{
+			".K",
+			".K",
+		},
+		[]string{
+			".K",
+			"K.",
+		},
+		[]string{
+			"KK.",
+			"...",
+			"...",
+		},
+		[]string{
+			"k",
+		},
+		[]string{
+			"K.K",
+			"...",
+			"...",
+		},
+		[]string{
+			"K..",
+			"...",
+			"K..",
+		},
+	}
+	for _, v := range trueTest {
+		if isOnlyKing(v) != true {
+			t.Errorf("error: expected true\n%s", strings.Join(v[:], "\n"))
+		}
+	}
+	for _, v := range falseTest {
+		if isOnlyKing(v) != false {
+			t.Errorf("error: expected false\n%s", strings.Join(v[:], "\n"))
+		}
+	}
+}
 
 func TestIsSquare(t *testing.T) {
 	tests := []struct {
