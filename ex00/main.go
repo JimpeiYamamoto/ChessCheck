e87b83db151c47e7e6339df9371f42d447d3aff4
JimpeiYamamoto <zinpei104@gmail.com>
2022-04-29 23:48:54 +0900

    add_isSquare

--- ex00/main.go
+++ ex00/main.go
@@ -1,6 +1,23 @@
 package main
 
+import (
+	"piscine"
+)
+
+func isSquare(board []string) bool {
+	len := piscine.Strslen(board)
+	for _, row := range board {
+		if len != piscine.Strlen(row) {
+			return false
+		}
+	}
+	return true
+}
+
 func checkmate(board []string) {
+	if !isSquare(board) {
+		return
+	}
 }
 
 func main() {
