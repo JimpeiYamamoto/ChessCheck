77745e7bf14843bb794202b8b25e42a5351f5e64
osorosia <64416398+osorosia@users.noreply.github.com>
2022-04-30 16:31:04 +0900

    Add: bishop test

--- ex00/main_test.go
+++ ex00/main_test.go
@@ -94,6 +94,7 @@ func TestIsSquare(t *testing.T) {
 
 func TestIsCheck(t *testing.T) {
 	testIsCheckForPawn(t)
+	testIsCheckForBishop(t)
 }
 
 func testIsCheckForPawn(t *testing.T) {
@@ -171,6 +172,125 @@ func testIsCheckForPawn(t *testing.T) {
 			"K..",
 			"...",
 			"..P",
+		},
+	}
+	for _, v := range trueTest {
+		if isCheck(v) != true {
+			t.Errorf("error: expected true\n%s", strings.Join(v[:], "\n"))
+		}
+	}
+	for _, v := range falseTest {
+		if isCheck(v) != false {
+			t.Errorf("error: expected false\n%s", strings.Join(v[:], "\n"))
+		}
+	}
+}
+
+func testIsCheckForBishop(t *testing.T) {
+	trueTest := [][]string{
+		[]string{
+			"K..",
+			".B.",
+			"...",
+		},
+		[]string{
+			"..K",
+			".B.",
+			"...",
+		},
+		[]string{
+			"...",
+			".B.",
+			"..K",
+		},
+		[]string{
+			"...",
+			".B.",
+			"K..",
+		},
+		[]string{
+			"..K",
+			"...",
+			"B..",
+		},
+		[]string{
+			"..B",
+			"...",
+			"K..",
+		},
+		[]string{
+			"K..",
+			"...",
+			"..B",
+		},
+		[]string{
+			"B..",
+			"...",
+			"..K",
+		},
+		[]string{
+			"BBB",
+			"BKB",
+			"BBB",
+		},
+	}
+	falseTest := [][]string{
+		[]string{
+			"B",
+		},
+		[]string{
+			".B",
+			"..",
+		},
+		[]string{
+			".K.",
+			".B.",
+			"...",
+		},
+		[]string{
+			"...",
+			".BK",
+			"...",
+		},
+		[]string{
+			"...",
+			".B.",
+			".K.",
+		},
+		[]string{
+			"...",
+			"KB.",
+			"...",
+		},
+		[]string{
+			"K..",
+			"..B",
+			"...",
+		},
+		[]string{
+			".B.",
+			"...",
+			"..K",
+		},
+		[]string{
+			"...",
+			"K.B",
+			"...",
+		},
+		[]string{
+			"...",
+			"B.K",
+			"...",
+		},
+		[]string{
+			".K.",
+			"...",
+			".B.",
+		},
+		[]string{
+			".B.",
+			"...",
+			".K.",
 		},
 	}
 	for _, v := range trueTest {
